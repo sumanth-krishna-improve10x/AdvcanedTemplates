0d94707ea4d572327bf0e18c023334fca3cb68d6
Sumanth krishna M <sumanth.krishna@improve10x.com>
2022-11-24 11:59:12 +0530

    created a splash Activity

---
+++ app/src/main/java/com/improve10x/advcanedtemplates/TemplateActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.advcanedtemplates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TemplateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_template);
+    }
+}
